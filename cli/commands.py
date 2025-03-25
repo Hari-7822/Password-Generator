1195fec453e14a76b2de95293f449153efe519db
Hari-7822 <h26058549@gmail.com>
2025-03-25 13:33:20 +0530

    cli-setup

--- cli/commands.py
+++ cli/commands.py
@@ -1,6 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser(description = "An addition program")
+parser = argparse.ArgumentParser(description = "Password Generator")
 parser.add_argument("add", nargs = '*', metavar = "num", type = int, help = "All the numbers separated by spaces will be added.")
 args = parser.parse_args()
 
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
 import random
 
 upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = [1,2,3,4,5,6,7,8,9,0]
-symbols = []
+symbols = ['!', '@', '#', '$', "%", '^', '&', "*", '(', ')', "_", '-', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'", '<', '>', "?", '.', '/' ]
 passkey =  upper + lower + numbers + symbols
 length = int(input("Enter the length : "))
 password = random.sample(passkey,length)
